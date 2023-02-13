1bc088ddddb316ec7419295fe5933b0cb6f9156c
kimyj0530 <k01059195317@gmail.com>
2023-02-13 11:28:32 +0900

    2023-02-13 Row, Column

--- contact_1/lib/main.dart
+++ contact_1/lib/main.dart
@@ -11,9 +11,14 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: Container(),
-        bottomNavigationBar: BottomAppBar( child: Text("Hello"),
+        body: Row( // Row:여러 위젯 가로로 배치하는 법 // 세로는 Column을 사용한다.
+          mainAxisAlignment: MainAxisAlignment.center, // 가로축 정렬
+          crossAxisAlignment: CrossAxisAlignment.center, // 세로축 정렬
+          children: [
+          Icon(Icons.star),
+          Icon(Icons.star),
+          Icon(Icons.star),
+        ],
       )
     )
     );
