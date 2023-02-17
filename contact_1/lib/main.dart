e2d84830f39ad9d3f2a9ee16c84da506f5a730a1
kimyj0530 <k01059195317@gmail.com>
2023-02-17 17:26:16 +0900

    2023-02-17 Flexible Expanded

--- contact_1/lib/main.dart
+++ contact_1/lib/main.dart
@@ -11,65 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-                onPressed: (){},
-                icon: Icon(Icons.search),
-                color: Colors.black
-            ),
-            IconButton(
-                onPressed: (){},
-                icon: Icon(Icons.menu),
-                color: Colors.black
-            ),
-            IconButton(
-                onPressed: (){},
-                icon: Icon(Icons.more_horiz),
-                color: Colors.black
-            ),
+        appBar: AppBar(),
+        body: Row(
+          children: [
+            Flexible(child: Container(color: Colors.blue), flex: 3),
+            Flexible(child: Container(color: Colors.green), flex: 7),
           ],
-          title: Row(
-            children: [
-              Text('금호동 3가',
-                  style: TextStyle(
-                      color: Colors.black)
-              ),
-              SizedBox(width: 8),
-              Icon(Icons.expand_more,
-                color: Colors.black,
-              ),
-              SizedBox(width: 8),
-            ],
-          ),
-          backgroundColor: Colors.white,
-        ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('images/camera.png', width: 150),
-              Container(
-                width: 300,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함'),
-                    Text('금호동 3가'),
-                    Text('7000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4'),
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
         )
       ),
     );
